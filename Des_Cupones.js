e5fa9f7a6eb885a8f08300e04f52aa45023497a7
leonline <infovillaonline@gmail.com>
2021-06-25 22:07:22 -0500

    #paila_lo_de_los_cupones

---
+++ Des_Cupones.js
@@ -0,0 +1,41 @@
+// const PrecioOrigen = 120;
+// const Descuento = 18;
+
+function calcularPrecioConDescuento(Precio, Descuento) {
+    const PorcentajePrecioDes = 100 - Descuento;
+    const PrecioConDes = (Precio * PorcentajePrecioDes) / 100; 
+    return PrecioConDes;
+}
+
+function BpriceDis(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = parseInt(inputPrice.value);
+    
+    const inputDis = document.getElementById("InputDiscount");
+    const DisValue = parseInt(inputDis.value);
+
+    const PrecioConDes = calcularPrecioConDescuento(priceValue, DisValue);
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son $" + PrecioConDes;
+}
+function BcuponDis(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = parseInt(inputPrice.value);
+    
+    const inputDis = document.getElementById("InputDiscount");
+    const DisValue = parseInt(inputDis.value);
+
+    const InputCupon =document.getElementById("InputCupon");
+    const priceCupon = parseInt(InputCupon.value);
+
+    const PrecioF = calcularPrecioConDescuento(priceValue, DisValue) * priceCupon;
+    alert(PrecioConDes);
+}
+
+
+// console.log({
+//     PrecioOrigen,
+//     Descuento,
+//     PorcentajePrecioDes,
+//     PrecioConDes
+// });
--- README.md
+++ README.md
@@ -15,3 +15,9 @@
 -2) Implementar la Formulas en JavaScrip
 -3) Crear  Funciones
 -4) Integrar JS con HTML
+
+## Taller #2.1 Prorcentajes, Descuentos y Cupones
+-1) Definir las Formulas
+-2) Implementar la Formulas en JavaScrip
+-3) Crear  Funciones
+-4) Integrar JS con HTML
