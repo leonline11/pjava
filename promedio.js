c05aad8c445343479fdba486fa456b55a4364f92
leonline <infovillaonline@gmail.com>
2021-06-29 20:57:18 -0500

    cambios

--- promedio.js
+++ promedio.js
@@ -1,6 +1,16 @@
-const lista1 = [100, 200, 300, 500, ];
-
-let sumaLista1 = 0;
-for (let i = 0; i < lista1.lenth; i++) {
-    sumaLista1 = sumaLista1 + Lista1[i];
-}
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //   sumaLista = sumaLista + lista[i];
+    // }
+  
+    const sumaLista = lista.reduce(
+      function (valorAcumulado = 0, nuevoElemento) {
+        return valorAcumulado + nuevoElemento;
+      }
+    );
+  
+    const promedioLista = sumaLista / lista.length;
+  
+    return promedioLista;
+  }
